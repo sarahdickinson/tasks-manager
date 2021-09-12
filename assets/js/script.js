15145b95b4158fd4320daf5a5cb8e969808dd1c6
Sarah Dickinson <sarahadickinson@gmail.com>
2021-09-11 23:40:33 -0400

    snapshot compare

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,24 +3,26 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var taskFormHandler = function(event) {
     event.preventDefault();
-    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskNameInput = document.querySelector("input[name='task-name'").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-    // check if input values are empty strings
-    if (!taskNameInput || !taskTypeInput) {
+    // check if inputs are empty (validate)
+    if (taskNameInput === "" || taskTypeInput === "") {
         alert("You need to fill out the task form!");
         return false;
-    };
+    }
 
     formEl.reset();
 
-    // package up data as an object
+    // reset form fields for next task to be entered
+    document.querySelector("input[name='task-name']").value = "";
+    document.querySelector("select[name='task-type']").selectedIndex = 0;
+
     var taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput
     };
 
-    // send it as an argument to createTaskEl
     createTaskEl(taskDataObj);
 };
 
@@ -35,7 +37,9 @@ var createTaskEl = function(taskDataObj) {
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);
 
-    // add entire list item to list
+    console.dir(listItemEl);
+
+    // add list item to list
     tasksToDoEl.appendChild(listItemEl);
 };
 
